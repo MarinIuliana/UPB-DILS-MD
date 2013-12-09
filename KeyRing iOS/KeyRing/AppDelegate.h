81a5b96310c631403ab6e22ed53d42fb4b414884
Tudor Codarnai <tudor.gnet@yahoo.ro>
2013-12-09 20:52:29 +0200

    iOS KeyRing first commit - mock-up

---
+++ KeyRing iOS/KeyRing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeyRing
+//
+//  Created by Tudor on 12/6/13.
+//  Copyright (c) 2013 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KeyRing iOS/KeyRing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KeyRing
+//
+//  Created by Tudor on 12/6/13.
+//  Copyright (c) 2013 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
